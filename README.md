f7a896b96564a6a5e4f56877e68419e411e5fa41
Yohann Assouline <yohannassouline@hotmail.fr>
2022-04-28 18:53:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# generate_bluetooth_16_bits_uuid_json
+
+A little script to generate a json file from the content of [this pdf](https://btprodspecificationrefs.blob.core.windows.net/assigned-values/16-bit%20UUID%20Numbers%20Document.pdf) that describes all ble 16-bits identifiers.
+
+I made this script to easily generate a dictionary to get identifier name from 16 bit raw bytes (for [this program](https://github.com/Yohannfra/ble_repl))
---
+++ generate_16_bits_uuid_json.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import sys
+import fitz
+import json
+
+ROW_FIRST_COLUMN = [
+    "16-bit UUID for Members",
+    "GATT Characteristic and Object Type",
+    "GATT Declarations ",
+    "GATT Descriptor",
+    "GATT Service",
+    "GATT Unit ",
+    "Protocol Identifier",
+    "SDO GATT Service",
+    "Service Classes and Profiles",
+]
+
+def main(argc, argv):
+    if argc != 2:
+        sys.exit("USAGE: ./generate_16_bits_uuid_json.py doc.pdf")
+
+    try:
+        doc = fitz.open(argv[1])
+    except:
+        sys.exit("Error opening file");
+
+
+    output = {
+    }
+    for k in ROW_FIRST_COLUMN:
+        output[k] = []
+
+    for page_number in range(doc.page_count):
+        page = doc.load_page(page_number)
+        text = page.get_text("text").split('\n')
+        for i in range(len(text)):
+            if text[i] in ROW_FIRST_COLUMN:
+                output[text[i]].append({text[i+1]: text[i+2]})
+
+    with open("ble_16_bits_uuids.json", "w") as outfile:
+        json.dump(output, outfile, indent=4)
+
+    print("OK")
+
+if __name__ == '__main__':
+    main(len(sys.argv), sys.argv)
